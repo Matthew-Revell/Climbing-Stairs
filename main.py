e0aefedd4e0d97c02e22cb850b75b420712755b0
Matthew-Revell <120280008+Matthew-Revell@users.noreply.github.com>
2022-12-12 05:33:04 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def climbStairs(self, n):
+        ways = [0] * (n + 1)
+        
+        ways[0] = 1
+        ways[1] = 1
+        
+        for i in range(2, n + 1):
+            ways[i] = ways[i - 1] + ways[i - 2]
+        
+        return ways[n]
